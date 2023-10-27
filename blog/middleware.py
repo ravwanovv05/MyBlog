7760ab033db948f96dddb87ff555193411b97aed
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-10-27 15:35:13 +0500

    Change

--- blog/middleware.py
+++ blog/middleware.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse, JsonResponse
-from django.views import View
+from django.http import HttpResponse
 
 
 class IPRestrictedMiddleware:
@@ -8,9 +7,9 @@ class IPRestrictedMiddleware:
 
     def __call__(self, request, *args, **kwargs):
         client_ip = request.META.get('REMOTE_ADDR')
+        print(client_ip)
         ip_address = [
-            '127.0.0.1',
-            '10.10.4.18'
+            '127.0.0.1'
         ]
         if client_ip not in ip_address and request.path.find('admin') != -1:
             return HttpResponse('Permission denied!')
